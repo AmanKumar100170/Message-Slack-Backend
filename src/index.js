d8203ddbeb1cbb09245f9c4bcfd5043d8295ac1f
Aman Kumar <aman100170@gmail.com>
2025-02-02 08:47:36 +0530

    Added basic routing layer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 
 import connectDB from './config/dbconfig.js';
 import { PORT } from './config/serverConfig.js';
+import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
