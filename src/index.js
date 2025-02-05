f2f510359e9472b680b210777a030f372f36d585
Aman Kumar <aman100170@gmail.com>
2025-02-05 12:46:53 +0530

    Integrated bullBoard setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import express from 'express';
 import connectDB from './config/dbconfig.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
+import bullServerAdapter from './config/bullBoardConfig.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/ui', bullServerAdapter.getRouter());
 
 app.use('/api', apiRouter);
 
