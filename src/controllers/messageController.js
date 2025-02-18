62e979e1c5c8269870a4af307d6753e83a2a4f29
Aman Kumar <aman100170@gmail.com>
2025-02-18 10:29:53 +0530

    Added api to get presigned url from aws s3

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
+import { s3 } from '../config/awsConfig.js';
+import { AWS_BUCKET_NAME } from '../config/serverConfig.js';
 import { getMessageService } from '../services/messageService.js';
 import { customErrorResponse, internalErrorResponse, successResponse } from '../utils/common/responseObjects.js';
 
@@ -16,6 +18,26 @@ export const getMessageController = async (req, res) => {
         return res.status(StatusCodes.OK).json(successResponse(messages, 'Messages fetched successfully'));
     } catch (error) {
         console.log('Get message controller error', error);
+        
+        if (error.statusCode) {
+            return res.status(error.statusCode).json(customErrorResponse(error));
+        }
+
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(internalErrorResponse(error));
+    }
+};
+
+export const getPresignedUrlFromAWS = async (req, res) => {
+    try {
+        const url = await s3.getSignedUrlPromise('putObject', {
+            Bucket: AWS_BUCKET_NAME,
+            Key: `${Date.now()}`,
+            Expires: 60
+        });
+
+        return res.status(StatusCodes.OK).json(successResponse(url, 'Presigned URL fetched successfully'));
+    } catch (error) {
+        console.log('Get presigned url from aws controller error', error);
         
         if (error.statusCode) {
             return res.status(error.statusCode).json(customErrorResponse(error));
--- src/routes/v1/messages.js
+++ src/routes/v1/messages.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { getMessageController } from "../../controllers/messageController.js";
+import { getMessageController, getPresignedUrlFromAWS } from "../../controllers/messageController.js";
 import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const router = express.Router();
+
+router.get('/pre-signed-url', isAuthenticated, getPresignedUrlFromAWS);
 
 router.get('/:channelId', isAuthenticated, getMessageController);
 
